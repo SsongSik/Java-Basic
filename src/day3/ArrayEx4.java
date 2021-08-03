2b15ce2fc92e340ae173e22807079c208c3a493f
Ssongsik <sgsk88@nate.com>
2021-08-03 23:50:28 +0900

    day3

---
+++ src/day3/ArrayEx4.java
@@ -0,0 +1,26 @@
+package day3;
+
+public class ArrayEx4 {
+
+	public static void main(String[] args) {
+		char[] abc = {'a', 'b', 'c', 'd'};
+		char[] num = {'1', '2' ,'3', '4', '5','6', '7', '8', '9'};
+		System.out.println(abc);
+		System.out.println(num);
+		
+		//배열 abc와 num을 붙혀서 하나의 배열 result로 만든다.
+		char[] result = new char[abc.length+num.length];
+		System.arraycopy(abc, 0, result, 0, abc.length);
+		System.arraycopy(num, 0, result, abc.length, num.length);
+		System.out.println(result);
+		
+		//배열 abc을 배열 num의 첫번째 위치부터 배열 abc의 길이만큼 복사
+		System.arraycopy(abc, 0, num, 0, abc.length);
+		System.out.println(num);
+		
+		//num의 인덱스 6 위치에 3개를 복사
+		System.arraycopy(abc, 0, num, 6, 3);
+		System.out.println(num);
+	}
+
+}
