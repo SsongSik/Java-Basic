3a63d306c5096af79cc2d690ddbd1ec0e7a9348e
Ssongsik <sgsk88@nate.com>
2021-08-11 17:19:30 +0900

    day6

---
+++ src/day6/Tv.java
@@ -0,0 +1,19 @@
+package day6;
+
+public class Tv {
+	String color;
+	boolean power;
+	int channel;
+	
+	void power() {
+		power = !power;
+	}
+	
+	void channelUp() {
+		++channel;
+	}
+	
+	void channelDown() {
+		--channel;
+	}
+}
