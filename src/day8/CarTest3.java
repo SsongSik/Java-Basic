7b4bb0dd4acc7a639ba1337917e414b568333678
Ssongsik <sgsk88@nate.com>
2021-08-13 19:38:59 +0900

    day8

---
+++ src/day8/CarTest3.java
@@ -0,0 +1,23 @@
+package day8;
+
+public class CarTest3 {
+
+	public static void main(String[] args) {
+		Car c1 = new Car();
+		Car c2 = new Car(c1);
+		
+		System.out.println("c1 : "+ c1.color+" "+ c1.geartype+" "+ c1.door);
+		System.out.println("c2 : "+ c2.color+" "+ c2.geartype+" "+ c2.door);
+		
+		c1.door = 100;
+		
+		System.out.println("c1 : "+ c1.color+" "+ c1.geartype+" "+ c1.door);
+		System.out.println("c2 : "+ c2.color+" "+ c2.geartype+" "+ c2.door);
+	}
+}
+/*°á°ú
+c1 : white auto 4
+c2 : white auto 4
+c1 : white auto 100
+c2 : white auto 4
+*/
