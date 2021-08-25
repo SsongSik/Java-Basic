d9c0258991c67496eb8b3ee358347372636ffb43
Ssongsik <sgsk88@nate.com>
2021-08-25 23:41:37 +0900

    day14

--- src/day14/Buyer.java
+++ src/day14/Buyer.java
@@ -1,8 +1,11 @@
 package day14;
 
+import java.util.Vector;
+
 public class Buyer {
 	int money = 1000;
 	int bonusPoint = 0;
+	Vector item = new Vector();
 	
 	void buy(Product p) {
 		if(money < p.price) {
@@ -12,6 +15,34 @@ public class Buyer {
 		
 		money -= p.price;
 		bonusPoint += p.bonusPoint;
+		item.add(p);
 		System.out.println(p+"을/를 구입하셨습니다.");
+	}
+	void summary() {
+		int sum = 0;
+		String itemList = "";
+		
+		if(item.isEmpty()) {
+			System.out.println("구매하신 제품이 없습니다.");
+			return;
+		}
+		
+		for(int i=0; i<item.size(); i++) {
+			Product p = (Product) item.get(i);
+			sum += p.price;
+			itemList += (i==0) ? "" + p : ", " + p;
+		}
+		System.out.println("구매하신 총 금액은 "+sum+"만원입니다");
+		System.out.println("구매하신 제품은 "+itemList+"입니다.");
+	}
+	void refund(Product p) {
+		if(item.remove(p)) {
+			money += p.price;
+			bonusPoint -= p.bonusPoint;
+			System.out.println(p+"를 반품하셨습니다.");
+		}
+		else {
+			System.out.println("구매하신 제품 중 해당 제품이 없습니다.");
+		}
 	}
 }
--- src/day14/PolyArgumentTest.java
+++ src/day14/PolyArgumentTest.java
@@ -4,11 +4,21 @@ public class PolyArgumentTest {
 
 	public static void main(String[] args) {
 		Buyer b = new Buyer();
-
-		b.buy(new Tv());
-		b.buy(new Computer());
+		Tv t = new Tv();
 		
-		System.out.println("현재 남은 돈은 "+b.money + "만원입니다.");
-		System.out.println("현재 보너스 점수는 "+b.bonusPoint+"점입니다.");
+		b.buy(t);
+		b.buy(new Computer());
+		b.refund(t);
+		b.summary();
+		
+//		System.out.println("현재 남은 돈은 "+b.money + "만원입니다.");
+//		System.out.println("현재 보너스 점수는 "+b.bonusPoint+"점입니다.");
 	}
 }
+/*결과
+Tv을/를 구입하셨습니다.
+Computer을/를 구입하셨습니다.
+Tv를 반품하셨습니다.
+구매하신 총 금액은 200만원입니다
+구매하신 제품은 Computer입니다.
+*/
