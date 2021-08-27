ba45018a22aa98a20749ac8fcf1cda768c762bb6
Ssongsik <sgsk88@nate.com>
2021-08-27 23:04:58 +0900

    day14

--- src/day14/PolyArgumentTest.java
+++ src/day14/PolyArgumentTest.java
@@ -10,9 +10,6 @@ public class PolyArgumentTest {
 		b.buy(new Computer());
 		b.refund(t);
 		b.summary();
-		
-//		System.out.println("현재 남은 돈은 "+b.money + "만원입니다.");
-//		System.out.println("현재 보너스 점수는 "+b.bonusPoint+"점입니다.");
 	}
 }
 /*결과
