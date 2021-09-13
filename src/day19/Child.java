84f52ef069c5e38414d6ee35bde683b2a537a3af
Ssongsik <sgsk88@nate.com>
2021-09-13 09:22:46 +0900

    testupdate

--- src/day19/Child.java
+++ src/day19/Child.java
@@ -2,6 +2,6 @@ package day19;
 
 public class Child extends Parent implements MyInterface, MyInterface2{
 	public void method1() {
-		System.out.println("method1() in Child");
+		System.out.println("method1() testin Child");
 	}
 }
